1041c7d4a72980ab05ae930c9fcacfa2dc709914
Kirill Vusov <kvusov@bk.ru>
2025-05-15 21:07:21 +0300

    Configured the repository

--- UserRepository/UserRepository.cs
+++ UserRepository/UserRepository.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contracts;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace UserRepository
 {
-    class UserRepository
+    class UserRepository : RepositoryBase, IUserRepository
     {
+        public UserRepository(UserRepositoryContext context) : base(context)
+        { }
+
+        public async Task<IEnumerable<User>> GetAllUsers(bool trackChanges)
+        {
+            return await FindAll(trackChanges)
+                .OrderBy(u => u.FirstName)
+                .ToListAsync();
+        }
+
+        public async Task<User> GetUser(Guid id, bool trackChanges)
+        {
+            return await FindByCondition(u => u.Id.Equals(id), trackChanges)
+                .SingleOrDefaultAsync();
+        }
+
+        public void CreateUser(User user)
+        {
+            CreateUser(user);
+        }
+
+        public void DeleteUser(User user)
+        {
+            DeleteUser(user);
+        }
     }
 }
--- UserRepository/UserRepositoryContext.cs
+++ UserRepository/UserRepositoryContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace UserRepository
 {
-    class UserRepositoryContext
+    public class UserRepositoryContext : DbContext
     {
+        public UserRepositoryContext(DbContextOptions opts) : base(opts)
+        { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
